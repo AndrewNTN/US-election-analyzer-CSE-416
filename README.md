c939387bd52a5f32e1a6916a65a87b459cb0ccb8
AndrewNTN <andrewton80@gmail.com>
2025-09-09 22:06:11 -0400

    init frontend, vite react ts tailwind prettier eslint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # US-election-analyzer-CSE-416
+
+See README in respective folders.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <header className="flex text-3xl justify-center">hi</header>
+    </>
+  );
+}
+
+export default App;
