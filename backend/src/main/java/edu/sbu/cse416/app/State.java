7b009232fa5387fe432d383a609e6754929fe811
AndrewNTN <andrewton80@gmail.com>
2025-09-17 16:50:36 -0400

    add formatter and reformat

--- backend/src/main/java/edu/sbu/cse416/app/State.java
+++ backend/src/main/java/edu/sbu/cse416/app/State.java
@@ -1,12 +1,21 @@
 package edu.sbu.cse416.app;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("states")
 public class State {
-  @Id public String id;      // "CA", "OR", "FL"
-  public String name;
-  public String teamTag;     // "LAKERS"
-  public State() {}
-  public State(String id, String name, String teamTag){ this.id=id; this.name=name; this.teamTag=teamTag; }
+    @Id
+    public String id; // "CA", "OR", "FL"
+
+    public String name;
+    public String teamTag; // "LAKERS"
+
+    public State() {}
+
+    public State(String id, String name, String teamTag) {
+        this.id = id;
+        this.name = name;
+        this.teamTag = teamTag;
+    }
 }
