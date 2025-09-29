211f6a410f45126457315679f621775961741a5a
AndrewNTN <andrewton80@gmail.com>
2025-09-29 17:39:29 -0400

    feat: added shadcn, tanstack router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import BaseMap from "./components/map-component";
-import "leaflet/dist/leaflet.css";
 
 export default function App() {
   return (
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="w-full bg-theme text-white shadow-md relative z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex items-center">
+            <h1 className="text-xl font-bold">US Election Analyzer</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
