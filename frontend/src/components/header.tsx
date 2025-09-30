f22093bffdf97ae9226395061df64e7e31effcd5
AndrewNTN <andrewton80@gmail.com>
2025-09-29 22:57:07 -0400

    feat: analysis drawer

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,14 @@
+import { Link } from "@tanstack/react-router";
+
 export default function Header() {
   return (
     <header className="w-full bg-theme text-white shadow-md relative z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <h1 className="text-xl font-bold">US Election Analyzer</h1>
+            <Link to="/">
+              <h1 className="text-xl font-bold">US Election Analyzer</h1>
+            </Link>
           </div>
         </div>
       </div>
