31d8881c40cfd0eb804f295b9d074dc69c9b7f46
dreamelax <Sibisatheeshk@gmail.com>
2025-10-22 14:10:57 -0400

    cleaned up
    cleaned up all unessecary backend test files and folders
    also removed api test files from frontend

--- backend/src/main/java/edu/sbu/cse416/app/model/State.java
+++
@@ -1,7 +0,0 @@
-package edu.sbu.cse416.app.model;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document("states")
-public record State(@Id String id, String name, String teamTag) {}
